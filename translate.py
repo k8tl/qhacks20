f07a0909245a732a35b14860370e6f6a148cce62
Katie Lu <katie.yg.lu@gmail.com>
2020-02-01 23:36:35 -0500

    Update translate.py

--- translate.py
+++ translate.py
@@ -1,51 +1,87 @@
-class Detect:
-    def detect_text(path):
-        """Detects text in the file."""
-        from google.cloud import vision
-        import io
+import io
+import os
+import html
+
+# Imports the Google Cloud client libraries
+from google.api_core.protobuf_helpers import get_messages
+from google.cloud import translate_v3beta1 as translate
+from google.cloud import vision
+from google.colab import drive
+
+drive.mount('/content/gdrive')
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/content/gdrive/My Drive/qhacks/qhacks2020-fca94e2d0136.json"
+
+class newTr:
+    def __init__(self, inFile):
+        self.img = inFile
+
+    def pic_to_text(self):
+        """Detects text in an image file
+        ARGS
+        infile: path to image file
+
+        RETURNS
+        String of text detected in image
+        """
+
+        # Instantiates a client
         client = vision.ImageAnnotatorClient()
 
-        with io.open('./schrift2', 'rb') as image_file:
+        # Opens the input image file
+        with io.open(self.img, 'rb') as image_file:
             content = image_file.read()
 
         image = vision.types.Image(content=content)
 
+        # For dense text, use document_text_detection
+        # For less dense text, use text_detection
         response = client.text_detection(image=image)
-        texts = response.text_annotations
-        print('Texts:')
+        text = response.full_text_annotation.text
 
-        for text in texts:
-            print('\n"{}"'.format(text.description))
+        return text
 
-            vertices = (['({},{})'.format(vertex.x, vertex.y)
-                        for vertex in text.bounding_poly.vertices])
+    def translate_text(self, text, source_language_code, target_language_code,
+                    project_id):
+        """Translates text to a given language using a glossary
 
-            print('bounds: {}'.format(','.join(vertices)))
+        ARGS
+        text: String of text to translate
+        source_language_code: language of input text
+        target_language_code: language of output text
+        project_id: GCP project id
+        glossary_name: name you gave your project's glossary
+            resource when you created it
 
-        if response.error.message:
-            raise Exception(
-                '{}\nFor more info on error messages, check: '
-                'https://cloud.google.com/apis/design/errors'.format(
-                    response.error.message))
-        return texts
+        RETURNS
+        String of translated text
+        """
 
-    def translate(texts):
-        from google.cloud import translate_v2 as translate
-        translate_client = translate.Client()
+        # Instantiates a client
+        client = translate.TranslationServiceClient()
 
-        if isinstance(text, six.binary_type):
-            text = text.decode('utf-8')
+        # Designates the data center location that you want to use
+        location = 'us-central1'
 
-        # Text can also be a sequence of strings, in which case this method
-        # will return a sequence of results for each text.
-        result = translate_client.translate(
-            text, target_language=target)
+        parent = client.location_path(project_id, location)
 
-        print(u'Text: {}'.format(result['input']))
-        print(u'Translation: {}'.format(result['translatedText']))
-        print(u'Detected source language: {}'.format(
-            result['detectedSourceLanguage']))
+        result = client.translate_text(
+            parent=parent,
+            contents=[text],
+            mime_type='text/plain',  # mime types: text/plain, text/html
+            source_language_code=source_language_code,
+            target_language_code=target_language_code)
+
+        print(result)
+        # Extract translated text from API response
+        return result
 
 def main():
-    newD = Detect()
-    newD.detect_text("/home/katie/Downloads/schrift.jpg")
+    # Photo from which to extract text
+    new = newTr('/content/gdrive/My Drive/qhacks/surveillance.jpeg')
+    # photo -> detected text
+    text_to_translate = new.pic_to_text()
+    print(text_to_translate)
+    new.translate_text(text_to_translate, 'fr', 'en', 'qhacks2020-86c07')
+
+if __name__=="__main__":
+    main()
